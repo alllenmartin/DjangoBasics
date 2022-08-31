62eea97caabe73cca0e7e9a1ab903356a21bf57a
alllenmartin <martinallen722@gmail.com>
2022-08-31 15:38:23 +0300

    Function Based API View

--- api_basics/views.py
+++ api_basics/views.py
@@ -10,7 +10,7 @@ from .serializers import ArticleSerializer
 def article_list(request):
 
 	if request.method == 'GET':
-		articles= Article.objects.all()
+		articles = Article.objects.all()
 		serializer = ArticleSerializer(articles, many=True)
 		return JsonResponse(serializer.data, safe=False)
 
@@ -23,3 +23,27 @@ def article_list(request):
 			return JsonResponse(serializer.data, status=201)
 
 		return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def article_detail(request, pk):
+	try:
+		article = Article.objects.get(pk=pk)
+	except Article.DoesNotExist:
+		return HttpResponse(status=404)
+
+	if request.method == 'GET':
+		serializer = ArticleSerializer(article)
+		return JsonResponse(serializer.data)
+
+	elif request.method == 'PUT':
+		data = JSONParser().parse(request)
+		serializer = ArticleSerializer(article, data=data)
+
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.errors, status=400)
+
+	elif request.method == 'DELETE':
+		article.delete()
+		return JsonResponse(status=204)
